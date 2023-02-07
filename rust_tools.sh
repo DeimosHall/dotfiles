9273f9aed3931a6c0115638f3db7f26395090059
Deimos <deimoshall@gmail.com>
2023-02-07 15:09:52 -0600

    fix: add the env of starship

--- rust_tools.sh
+++ rust_tools.sh
@@ -5,12 +5,14 @@ sudo apt upgrade -y
 sudo apt install build-essential -y
 sudo apt install cmake -y
 sudo apt install curl -y
-sudo apt-get install libfontconfig libfontconfig1-dev
+sudo apt-get install libfontconfig libfontconfig1-dev -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "Reboot your system and run this script again"
+
 cargo install bottom
 cargo install starship --locked
-echo 'eval "$(starship init bash)"' >> .bashrc 
+echo 'eval "$(starship init bash)"' >> ~/.bashrc 
 
 cargo install bat --locked
 echo "alias cat='bat'" >> ~/.bashrc
