750a564b5aa8691cf3c9427b35d455a11b141b88
Deimos <deimoshall@gmail.com>
2023-03-18 10:31:33 -0600

    feat: add net-tools

---
+++ system_tools.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install speedtest-cli -y
+sudo apt install python3-pip -y
+pip install pyserial
+sudo apt install obs-studio -y
+sudo apt install net-tools -y
